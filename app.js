b90dee8b5456451ff0efd2ac72e9366c204430ff
flamesabor <vysakhul@gmail.com>
2018-10-16 17:29:36 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+//Port
+const port = 8080;
+
+//Init App
+
+const app=express();
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const url = 'mongodb://admin:admin-123@ds233323.mlab.com:33323/reviewbase';
+
+
+//Body-Parser middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//View setup
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+
+//Connect to mongodb
+MongoClient.connect(url,(err,database)=>{
+console.log('MongoDB Connected.....');
+if(err) throw err;
+
+db = database;
+Todos= db.collection('todos');
+
+app.listen(port, ()=>{
+    console.log('Server running on port ' + port );
+});
+
+});
+
+
+app.get('/', (req, res, next)=>{
+    Todos.find({}).toArray((err, todos)=>{
+        if(err){
+            return console.log(err);
+        }
+        
+        res.render('index', {
+            todos: todos
+        });
+    });
+});
+
+
+app.post('/todo/add', (req, res, next)=>{
+//Create todo
+const todo = {
+    text: req.body.text,
+    body: req.body.body
+}
+
+//Insert Todo
+Todos.insert(todo, (err, result)=>{
+if(err){
+    return console.log(err);
+}
+console.log('Todo Added.....');
+res.redirect('/');
+});
+});
+
+app.delete('/todo/delete/:id', (req, res, next)=>{
+const query = {_id: ObjectID(req.params.id)}
+Todos.deleteOne(query, (err, response)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log('Todo Removed');
+    res.send(200);
+});
+});
+
+
+app.get('/todo/edit/:id', (req, res, next)=>{
+    const query = {_id: ObjectID(req.params.id)}
+    Todos.find(query).next((err, todo)=>{
+        if(err){
+            return console.log(err);
+        }
+        
+        res.render('edit', {
+            todo: todo
+        });
+    });
+});
+
+
+app.post('/todo/edit/:id', (req, res, next)=>{
+    const query = {_id: ObjectID(req.params.id)}
+    //Create todo
+    const todo = {
+        text: req.body.text,
+        body: req.body.body
+    }
+    
+    //Update Todo
+    Todos.updateOne(query, {$set:todo}, (err, result)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log('Todo Updated.....');
+    res.redirect('/');
+    });
+    });
+
+
+    //Render new Topics page
+
+    app.get('/add', (req, res, next)=>{
+        Todos.find({}).toArray((err, todos)=>{
+            if(err){
+                return console.log(err);
+            }
+            
+            res.render('addTopic', {
+                todos: todos
+            });
+        });
+    });
