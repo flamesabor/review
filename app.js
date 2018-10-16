bb95edf5ff49283fd9e4878abe12258b75a5e2c2
flamesabor <vysakhul@gmail.com>
2018-10-16 18:04:03 +0530

    sIXTH Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 //Port
-const port = 8080;
+var port = process.env.PORT || 3000;
 
 //Init App
 
